3ae05007229c66cf66d0677684e3b99e6ed9f17b
Martin Beroiz <martinberoiz@gmail.com>
2020-10-23 10:34:59 -0500

    Added boilerplate for regression tests

---
+++ tests/test_rompy_to_arby.py
@@ -0,0 +1,13 @@
+import unittest
+import arby
+
+class TestBessel(unittest.TestCase):
+    def setUp(self):
+        ...
+
+    def test_bessel_consistent(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
