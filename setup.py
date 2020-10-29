c9afaa058d4aaa15b8679ffc5f3f595fc1a068e1
Martin Beroiz <martinberoiz@gmail.com>
2020-10-29 17:56:47 -0500

    Grab version from init instead of hard-coding in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,19 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+arby_init_path = os.path.join(BASE_DIR, "arby", "__init__.py")
+
+with open(arby_init_path, "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            _, _, ARBY_VERSION = line.replace('"', "").split()
+            break
+
+
 setup(
     name="arby",
-    version="0.1",
+    version=ARBY_VERSION,
     # description="Short description",
     long_description=long_description,
     long_description_content_type="text/markdown",
