2d2ebaa00f7621744962ea550fe4f4ba872458f5
Martin Beroiz <martinberoiz@gmail.com>
2020-10-29 18:08:07 -0500

    Added very simple trivial test for the bessel regression test

--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -1,12 +1,15 @@
 import unittest
 import arby
+import numpy as np
 
-class TestBessel(unittest.TestCase):
+class TestBesselExample(unittest.TestCase):
     def setUp(self):
-        ...
+        basis = np.loadtxt("tests/bessel/bessel_basis.txt")
+        training = np.loadtxt("tests/bessel/bessel_training.txt")
 
     def test_bessel_consistent(self):
-        ...
+        self.assertEqual(basis.shape, (10, 101))
+        self.assertEqual(training.shape, (101, 101))
 
 
 if __name__ == "__main__":
