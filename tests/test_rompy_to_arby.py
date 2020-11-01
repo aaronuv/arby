726fbec174fa7470f40b2405f05b0166ee640e5c
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-11-01 12:53:36 -0300

    input training bessel

--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -9,13 +9,14 @@ class TestBesselExample(unittest.TestCase):
         self.training = np.loadtxt("tests/bessel/bessel_training.txt")
 
     def test_bessel_regression(self):
-        nu = np.linspace(0, 10, num=101)
+#        nu = np.linspace(0, 10, num=101)
         #set integration rule
         integration = arby.integrals.Integration([0,1], num=101, rule='riemann')
         #set integration nodes
-        x = integration.nodes
+#        x = integration.nodes
         #build traning space
-        training = np.array([BesselJ(nn, x) for nn in nu])
+#        training = np.array([BesselJ(nn, x) for nn in nu])
+        training = self.training
         #build reduced basis
         rb = arby.greedy.ReducedBasis(integration)
         rb.make(training, 0, 1e-14, verbose=False)
