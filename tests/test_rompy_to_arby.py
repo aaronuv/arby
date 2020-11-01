f14fff647b4106328a22b0cc4c1e940f077584a8
Martin Beroiz <martinberoiz@gmail.com>
2020-11-01 10:19:33 -0600

    Cleaned up regression test

--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -1,7 +1,7 @@
 import unittest
 import arby
 import numpy as np
-from scipy.special import jv as BesselJ
+
 
 class TestBesselExample(unittest.TestCase):
     def setUp(self):
@@ -9,17 +9,14 @@ class TestBesselExample(unittest.TestCase):
         self.training = np.loadtxt("tests/bessel/bessel_training.txt")
 
     def test_bessel_regression(self):
-#        nu = np.linspace(0, 10, num=101)
+        "Test that the reduced basis matches ROMpy's for the same training data"
+
+        nbasis, npoints = self.basis.shape
         #set integration rule
-        integration = arby.integrals.Integration([0,1], num=101, rule='riemann')
-        #set integration nodes
-#        x = integration.nodes
-        #build traning space
-#        training = np.array([BesselJ(nn, x) for nn in nu])
-        training = self.training
+        integration = arby.integrals.Integration([0,1], num=npoints, rule='riemann')
         #build reduced basis
         rb = arby.greedy.ReducedBasis(integration)
-        rb.make(training, 0, 1e-14, verbose=False)
+        rb.make(self.training, 0, 1e-14, verbose=False)
         #compare
         self.assertTrue(np.allclose(rb.basis,self.basis,rtol=1e-5,atol=1e-8))
 
