56bc4a3aaa858c4a6db925a147282f8e14a6031f
Martin Beroiz <martinberoiz@gmail.com>
2020-11-01 08:37:56 -0600

    added assert in test

--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -8,11 +8,6 @@ class TestBesselExample(unittest.TestCase):
         self.basis = np.loadtxt("tests/bessel/bessel_basis.txt")
         self.training = np.loadtxt("tests/bessel/bessel_training.txt")
 
-    def test_bessel_consistent(self):
-        self.assertEqual(self.basis.shape, (10, 101))
-        self.assertEqual(self.training.shape, (101, 101))
-
-
     def test_bessel_regression(self):
         nu = np.linspace(0, 10, num=101)
         #set integration rule
@@ -23,10 +18,10 @@ class TestBesselExample(unittest.TestCase):
         training = np.array([BesselJ(nn, x) for nn in nu])
         #build reduced basis
         rb = arby.greedy.ReducedBasis(integration)
-        rb.make(training, 0, 1e-14, verbose=True)
+        rb.make(training, 0, 1e-14, verbose=False)
         #compare
-        #atol=1e-8 and rtol=1e-5 are default values
-        np.allclose(rb.basis,self.basis)
+        self.assertTrue(np.allclose(rb.basis,self.basis,rtol=1e-5,atol=1e-8))
+
         
 if __name__ == "__main__":
     unittest.main()
