250d88e3923dff45be9d944e77c81a7bd5888386
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-11-05 00:59:41 -0300

    add test eim

--- arby/__init__.py
+++ arby/__init__.py
@@ -5,3 +5,4 @@ __version__ = "0.1a1"
 from .lib import *
 from .integrals import *
 from .greedy import *
+from .eim import *
--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -8,7 +8,7 @@ class TestBesselExample(unittest.TestCase):
         self.basis = np.loadtxt("tests/bessel/bessel_basis.txt")
         self.training = np.loadtxt("tests/bessel/bessel_training.txt")
 
-    def test_bessel_regression(self):
+    def test_regression_reduced_basis(self):
         "Test that the reduced basis matches ROMpy's for the same training data"
 
         nbasis, npoints = self.basis.shape
@@ -24,6 +24,17 @@ class TestBesselExample(unittest.TestCase):
             np.allclose(rb.basis, self.basis, rtol=1e-5, atol=1e-8)
         )
 
+    def test_regression_EIM(self):
+        "Test that EIM matches ROMpy's for the same training data"
+        
+        rompy_eim_nodes = np.array([0, 100, 2, 36, 9, 72, 1, 20, 89, 4])
+        # Compute eim nodes for Bessel functions
+        eim_bessel = arby.eim.EmpiricalMethods(self.basis)
+        eim_bessel.eim()
+        # compare
+        self.assertTrue(
+            (rompy_eim_nodes == eim_bessel.indices).all()
+        )
 
 if __name__ == "__main__":
     unittest.main()
