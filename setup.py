b26e1e27f383abfd2032182dda0149c776b7595a
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-11-14 00:42:59 -0300

    acento

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # description="Short description",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Aarón Villanueva",
+    author="Aaron Villanueva",
     author_email="aaron.villanueva@unc.edu.ar",
     url="https://gitlab.com/aaronuv/arby",
     packages=[
