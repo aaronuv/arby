96fd9d4cdf18f8eb322a3f146bb4552a3f86aa83
Martin Beroiz <martinberoiz@gmail.com>
2020-11-18 16:24:58 -0600

    Added some documentation. Work in progress.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'arby')))
 
 
 # -- Project information -----------------------------------------------------
@@ -21,6 +21,16 @@ project = 'Arby'
 copyright = '2020, Aarón Villanueva'
 author = 'Aarón Villanueva'
 
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# Get the version from astroalign file itself (not imported)
+with open(os.path.join('..', '..', 'arby', '__init__.py'), 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            _, _, __version__ = line.replace('"', "").split()
+            break
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,6 +38,8 @@ author = 'Aarón Villanueva'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +56,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,18 +3,20 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Arby's documentation!
-================================
+Arby
+====
+
+`Model Order Reduction (MOR) <https://en.wikipedia.org/wiki/Model_order_reduction>`_ is
+a technique for reducing the computational complexity of mathematical models in numerical simulations.
+
+Arby is a fully data-driven Python module to construct reduced order basis,
+empirical interpolants and surrogate models from training data.
+
+
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Contents
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   installation
+   tutorial
