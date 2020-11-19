2170f9d42c14533ad9f48643addb7cd8e7f6987e
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-11-19 01:07:18 -0300

    clean odd methods/refactoring

--- arby/greedy.py
+++ arby/greedy.py
@@ -3,20 +3,16 @@
 # Copyright (c) 2020, Aarón Villanueva
 # License: MIT
 #   Full Text: https://gitlab.com/aaronuv/arby/-/blob/master/LICENSE
-"""
-Classes for building reduced basis greedy algorithms
-"""
 
 import numpy as np
 from .integrals import Integration
 from random import randint  # noqa: F401
 
 
-#############################################
-# Class for iterated, modified Gram-Schmidt #
-#      orthonormalization of functions      #
-#############################################
-
+# ==========================================
+# Class for Iterated-Modified Gram-Schmidt
+#      Orthonormalization Method
+# ==========================================
 
 def GS_add_element(h, basis, integration, a, max_iter):
     """
@@ -46,9 +42,8 @@ def GS_add_element(h, basis, integration, a, max_iter):
 
 
 def GramSchmidt(vectors, integration, a=0.5, max_iter=3):
-    """Class for building an orthonormal basis using the
-    iterated, modified Gram-Schmidt procedure.
-
+    """Class for building an orthonormal basis using the iterated,
+    modified Gram-Schmidt procedure.
     Input
     -----
     vectors: set of vectors to orthonormalize
@@ -80,10 +75,9 @@ def GramSchmidt(vectors, integration, a=0.5, max_iter=3):
     return basis
 
 
-#############################################
-# Class for reduced basis greedy algorithms #
-#############################################
-
+# ===============================
+# Class for Reduced Basis Method
+# ===============================
 
 class ReducedBasis:
     """Class for standard reduced basis greedy algorithm."""
@@ -91,8 +85,9 @@ class ReducedBasis:
     def __init__(self, training_space, interval, rule="riemann"):
         self.training = np.array(training_space)
         self.Ntrain, self.Nsamples = training_space.shape
-        self.loss = self.proj_errors_from_alpha
-        self.integration = Integration(interval=interval, num=self.Nsamples,
+        self.loss = self.projection_error
+        self.integration = Integration(interval=interval,
+                                       num=self.Nsamples,
                                        rule=rule)
 
     def build_rb(self, index_seed=0, tol=1e-12, verbose=False):
@@ -105,16 +100,16 @@ class ReducedBasis:
             seed_function = self.training[index_seed]
 
         # ====== Seed the greedy algorithm and allocate memory ================
+
         # Validate inputs
         assert self.Nsamples == np.size(
             self.integration.weights
         ), "Number of samples is inconsistent with quadrature rule."
 
         # Allocate memory for greedy algorithm arrays
-        dtype = type(np.asarray(self.training).flatten()[0])
-        self.allocate(self.Ntrain, self.Nsamples, dtype=dtype)
+        self.allocate(self.Ntrain, self.Nsamples, dtype=self.training.dtype)
 
-        # Compute norms of training space data
+        # Compute norms of the training space data
         self._norms = np.array([self.integration.norm(tt)
                                for tt in self.training])
 
@@ -122,28 +117,29 @@ class ReducedBasis:
         self.indices[0] = index_seed
         self.basis[0] = self.training[index_seed] / self._norms[index_seed]
         self.basisnorms[0] = self._norms[index_seed]
-        self.alpha[0] = self.integration.dot(self.basis[0], self.training)
-        # =====================================================================
+        self.proj_matrix[0] = self.integration.dot(self.basis[0],
+                                                   self.training)
 
-        # ===== Start greedy loops ============================================
+        # ===== Start greedy loop =============================================
+
         if verbose:
-            print("\nStep", "\t", "Error")
+            print("\n Step", "\t", "Error")
 
         nn = 0
         sigma = 1.0
         while sigma > tol:
             nn += 1
-            errs = self.loss(self.alpha[:nn], norms=self._norms)
+            errs = self.loss(self.proj_matrix[:nn], norms=self._norms)
             next_index = np.argmax(errs)
 
             if next_index in self.indices:
                 self.size = nn - 1
                 self.trim(self.size)
-                raise Exception("Index already selected: Exiting"
+                raise Exception("Index already selected: exiting"
                                 "greedy algorithm.")
             else:
                 self.indices[nn] = next_index
-                self.errors[nn - 1] = errs[next_index]
+                self.errors[nn-1] = errs[next_index]
                 self.basis[nn], self.basisnorms[nn] = GS_add_element(
                     self.training[self.indices[nn]],
                     self.basis[:nn],
@@ -151,8 +147,8 @@ class ReducedBasis:
                     a=0.5,
                     max_iter=3,
                 )
-                self.alpha[nn] = self.integration.dot(self.basis[nn],
-                                                      self.training)
+                self.proj_matrix[nn] = self.integration.dot(self.basis[nn],
+                                                            self.training)
             sigma = errs[next_index]
             if verbose:
                 print(nn, "\t", sigma)
@@ -168,55 +164,39 @@ class ReducedBasis:
         self.indices = np.empty(Npoints, dtype="int")
         self.basis = np.empty((Npoints, Nquads), dtype=dtype)
         self.basisnorms = np.empty(Npoints, dtype="double")
-        self.alpha = np.empty((Npoints, Npoints), dtype=dtype)
+        self.proj_matrix = np.empty((Npoints, Npoints), dtype=dtype)
 
-    def proj_error_from_basis(self, basis, h_vector):
-        """Square of the projection error of a function h on basis"""
-        h_vector_sqnorm = self.integration.norm(h_vector).real ** 2
-        inner_prod = np.array(
-            [self.integration.dot(basis_elem, h_vector) ** 2
-             for basis_elem in basis]
-        )
-        return h_vector_sqnorm - np.linalg.norm(inner_prod)
-
-    def proj_errors_from_basis(self, basis, hs):
-        """Square of the projection error of functions hs on basis"""
-        return [self.proj_error_from_basis(basis, hh) for hh in hs]
-
-    def proj_errors_from_alpha(self, alpha, norms=None):
+    def projection_error(self, proj_matrix, norms):
         """Square of the projection error of a function h on basis in terms
-        of pre-computed alpha matrix"""
-        if norms is None:
-            norms = np.ones(len(alpha[0]), dtype="double")
-        ans = 0.0
-        for aa in alpha:
-            ans += np.abs(aa) ** 2
-        return norms ** 2 - ans
-
-    def projection_from_basis(self, h, basis):
-        """Project a function h onto the basis functions"""
-        ans = 0.0
-        for ee in basis:
-            ans += ee * self.integration.dot(ee, h)
-        return ans
-
-    def projection_from_alpha(self, alpha, basis):
-        """Project a function h onto the basis functions using the
-        precomputed quantity alpha = <basis, h>"""
-        ans = 0.0
-        for ii, ee in basis:
-            ans += ee * alpha[ii]
-        return ans
-
-    def _Alpha(self, E, e, alpha):
-        return self.integration.dot(E, self.projection_from_alpha(alpha, e))
-
-    def Alpha_arr(self, E, e, alpha):
-        return np.array([self._Alpha(EE, e, alpha) for EE in E])
+        of pre-computed projection matrix"""
+        proj_norms = np.array([np.linalg.norm(proj_matrix[:, i])
+                               for i in range(self.Ntrain)]
+                              )
+        return norms ** 2 - proj_norms**2
 
     def trim(self, num):
         """Trim arrays to have size num"""
         self.errors = self.errors[:num]
         self.indices = self.indices[:num]
         self.basis = self.basis[:num]
-        self.alpha = self.alpha[:num]
+        self.proj_matrix = self.proj_matrix[:num]
+
+    # ==== Validation functions ===============================================
+
+    # This function inherites homology operation property from .dot method from
+    # from integrals.py. Then h_vector may be an array of functions.
+    def proj_error_from_basis(self, basis, h_vector):
+        """Square of the projection error of a function h_vector on basis."""
+        h_vector_sqnorm = self.integration.norm(h_vector).real
+        inner_prod = np.array(
+            [self.integration.dot(basis_elem, h_vector)
+             for basis_elem in basis]
+        )
+        return h_vector_sqnorm**2 - np.linalg.norm(inner_prod)**2
+
+    def project_on_basis(self, h, basis):
+        """Project a function h onto the basis functions"""
+        projected_function = 0.0
+        for e in basis:
+            projected_function += e * self.integration.dot(e, h)
+        return projected_function
