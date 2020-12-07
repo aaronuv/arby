cef120039053884a34fcb1e7b9c97622ea0ae40f
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-12-06 23:42:28 -0300

    bump arby to version

--- arby/__init__.py
+++ arby/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
-__version__ = "0.1a1"
+__version__ = "0.1"
 
 from .integrals import Integration
 from .core import ReducedOrderModeling, gram_schmidt
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,13 +12,9 @@
 #
 import os
 import sys
-#import mock
 
 sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 
-#MOCK_MODULES = ['numpy', 'scipy','scipy.interpolate.splrep','scipy.interpolate.splev', 'logging', 'random.randint']
-#for mod_name in MOCK_MODULES:
-#    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
