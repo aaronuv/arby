7553b76d795cc41d9daea52bb2ae8772414642d4
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-12-07 04:10:38 +0000

    Update __init__.py

--- arby/__init__.py
+++ arby/__init__.py
@@ -1,10 +1,11 @@
 # __init__.py
 
-"""Model Order Reduction (MOR)
-is a technique for reducing the computational complexity of
-mathematical models in numerical simulations.
-Arby is a fully data-driven Python module to construct reduced bases,
-empirical interpolants and surrogate models from training data.
+"""Model Order Reduction (MOR) is a technique for reducing the computational 
+the complexity of mathematical models in numerical simulations.
+
+Arby is a fully data-driven Python module to construct reduced bases, empirical 
+interpolants and surrogate models from training data.
+
 """
 
 __version__ = "0.1"
