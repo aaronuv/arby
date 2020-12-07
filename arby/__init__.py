8342cd9e8bc089eb605bc34f047e6c823ac4f2a5
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-12-07 04:03:30 +0000

    Update __init__.py

--- arby/__init__.py
+++ arby/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1"
 
+__all__ = ["Integration", "ReducedOrderModeling", "gram_schmidt"]
+
 from .integrals import Integration
 from .core import ReducedOrderModeling, gram_schmidt
-
-__all__ = ["Integration", "ReducedOrderModeling", "gram_schmidt"]
