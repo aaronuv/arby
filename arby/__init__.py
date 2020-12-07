e86fa3f808bd1558e98a1fac637ffffe4d3323c4
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2020-12-07 14:45:11 -0300

    fix style iter2

--- arby/__init__.py
+++ arby/__init__.py
@@ -17,5 +17,5 @@ __version__ = "0.1"
 
 __all__ = ["Integration", "ReducedOrderModeling", "gram_schmidt"]
 
-from .integrals import Integration
 from .core import ReducedOrderModeling, gram_schmidt
+from .integrals import Integration
