9b5ad96226cca3b66356e2f53cb7b9767838ecc6
Aarón Villanueva <aaron.villanueva@unc.edu.ar>
2021-03-22 14:04:11 -0300

    embed pendulum notebook

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,9 @@ with open(os.path.join("..", "..", "arby", "__init__.py"), "r") as f:
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon"
-]
+    "sphinx.ext.napoleon",
+    "nbsphinx"
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -49,7 +50,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
