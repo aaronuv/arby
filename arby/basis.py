25467593907681c387164f36b18500828d44479b
JuanBC <jbc.develop@gmail.com>
2021-03-29 16:36:35 -0300

    basis new api

---
+++ arby/basis.py
@@ -0,0 +1,197 @@
+# - rom.py -
+
+# Copyright (c) 2020, Aarón Villanueva
+# License: MIT
+#   Full Text: https://gitlab.com/aaronuv/arby/-/blob/master/LICENSE
+
+"""Basis analysis class."""
+
+import functools
+import logging
+from collections import namedtuple
+
+import attr
+
+import numpy as np
+
+from .integrals import Integration
+
+# ================
+# Constants
+# ================
+
+if hasattr(functools, "cached_property"):
+    cached_property = functools.cached_property
+else:
+    def cached_property(func):  # pragma: no cover
+        """Workaround for functools.cached_property for Python < 3.8."""
+        cache = functools.lru_cache(maxsize=None)
+        cached_func = cache(func)
+        cached_prop = property(cached_func)
+        return cached_prop
+
+
+logger = logging.getLogger("arby.basis")
+
+
+# =================================
+# Class for Basis Analysis
+# =================================
+
+#: Simple container of the Empirical Interpolantion Matrix
+EIM = namedtuple("EIM", ["interpolant", "nodes"])
+
+
+@attr.s(frozen=True, hash=False)
+class Basis:
+    """Basis elements utilities.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        The reduced basis of the Reduced Order Model.
+    integration: arby.integrals.Integration
+        Instance of the `Integration` class.
+
+    Attributes
+    ----------
+    Nbasis_: int
+        Number of basis elements.
+
+
+    References
+    ----------
+    .. [field2014fast] Scott E. Field, Chad R. Galley, Jan S. Hesthaven,
+        Jason Kaye, and Manuel Tiglio. Fast Prediction and Evaluation of
+        Gravitational Waveforms Using Surrogate Models. Phys. Rev. X 4, 031006
+
+    """
+
+    data: np.ndarray = attr.ib(converter=np.asarray)
+    integration: np.ndarray = attr.ib(
+        validator=attr.validators.instance_of(Integration)
+    )
+    Nbasis_: int = attr.ib(init=False)
+
+    # ==== Attrs orchestration=================================================
+
+    @Nbasis_.default
+    def _Nbasis__default(self):
+        return self.data.shape[0]
+
+    # ====== Empirical Interpolation Method ===================================
+
+    def _next_vandermonde(self, data, nodes, vandermonde=None):
+        """Build the next Vandermonde matrix from the previous one."""
+        if vandermonde is None:
+            vandermonde = [[data[0, nodes[0]]]]
+            return vandermonde
+
+        n = len(vandermonde)
+        new_node = nodes[-1]
+        for i in range(n):
+            vandermonde[i].append(data[i, new_node])
+
+        vertical_vector = [data[n, nodes[j]] for j in range(n)]
+        vertical_vector.append(data[n, new_node])
+        vandermonde.append(vertical_vector)
+        return vandermonde
+
+    @cached_property
+    def eim_(self):
+        """Empirical Interpolantion matrix.
+
+        Implement the Empirical Interpolation Method [field2014fast]_ to select
+        a set of interpolation nodes from the physical interval and build an
+        interpolant matrix.
+
+        Raises
+        ------
+        ValueError
+            If there is no basis for EIM.
+
+        """
+        nodes = []
+        v_matrix = None
+        first_node = np.argmax(np.abs(self.data[0]))
+        nodes.append(first_node)
+
+        logger.debug(first_node)
+
+        for i in range(1, self.Nbasis_):
+            v_matrix = self._next_vandermonde(self.data, nodes, v_matrix)
+            base_at_nodes = [self.data[i, t] for t in nodes]
+            invV_matrix = np.linalg.inv(v_matrix)
+            step_basis = self.data[:i]
+            basis_interpolant = base_at_nodes @ invV_matrix @ step_basis
+            residual = self.data[i] - basis_interpolant
+            new_node = np.argmax(abs(residual))
+
+            logger.debug(new_node)
+
+            nodes.append(new_node)
+
+        v_matrix = np.array(self._next_vandermonde(self.data, nodes, v_matrix))
+        invV_matrix = np.linalg.inv(v_matrix.transpose())
+        interpolant = self.data.transpose() @ invV_matrix
+
+        return EIM(interpolant=interpolant, nodes=nodes)
+
+    def projection_error(self, h, basis):
+        """Square of the projection error of a function onto a basis.
+
+        The error is computed in the L2 norm.
+
+        Parameters
+        ----------
+        h: numpy.array
+            Function or set of functions to be projected.
+        basis: numpy.array
+            Orthonormal basis.
+
+        Returns
+        -------
+        l2_error: float or numpy.array
+            Square of the projection error.
+        """
+        h_norm = self.integration.norm(h).real
+        inner_prod = np.array(
+            [self.integration.dot(basis_elem, h) for basis_elem in basis]
+        )
+        l2_error = h_norm ** 2 - np.linalg.norm(inner_prod) ** 2
+        return l2_error
+
+    def project(self, h):
+        """Project a function h on the basis.
+
+        Parameters
+        ----------
+        h: numpy.array
+            Function or set of functions to be projected.
+
+        Returns
+        -------
+        projected_function: numpy.array
+            Projection of h on the given basis.
+        """
+        projected_function = 0.0
+        for e in self.data:
+            projected_function += e * self.integration.dot(e, h)
+        return projected_function
+
+    def interpolate(self, h):
+        """Interpolate a function h at EIM nodes.
+
+        Parameters
+        ----------
+        h: numpy.array
+            Function or set of functions to be interpolated.
+
+        Returns
+        -------
+        h_interpolated: numpy.array
+            Function h interpolated at EIM nodes.
+        """
+        h_at_nodes = np.array([h[eim_node] for eim_node in self.eim_.nodes])
+        h_interpolated = self.eim_.interpolant @ h_at_nodes
+        return h_interpolated
---
+++ tests/test_basis.py
@@ -0,0 +1,122 @@
+# test_arby.py
+
+# Copyright (c) 2020, Aarón Villanueva
+# License: MIT
+#   Full Text: https://gitlab.com/aaronuv/arby/-/blob/master/LICENSE
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+import arby
+
+import numpy as np
+
+import pytest
+
+from scipy.special import jv as BesselJ
+
+
+# =============================================================================
+# TESTS
+# =============================================================================
+
+
+def test_basis_shape(basis_data, physical_interval):
+    integration = arby.Integration(physical_interval)
+    basis = arby.Basis(basis_data, integration)
+    assert basis.Nbasis_ == len(basis_data)
+
+
+def test_eim(basis_data, physical_interval):
+    integration = arby.Integration(physical_interval)
+    basis = arby.Basis(basis_data, integration)
+    np.testing.assert_allclose(basis.eim_.interpolant.mean(), 0.1)
+    np.testing.assert_allclose(basis.eim_.interpolant.std(), 0.345161095)
+    np.testing.assert_array_equal(
+        basis.eim_.nodes, [0, 100, 2, 36, 9, 72, 1, 20, 89, 4]
+    )
+
+
+def test_project(basis_data, physical_interval):
+    integration = arby.Integration(physical_interval)
+    basis = arby.Basis(basis_data, integration)
+
+    project = basis.project(physical_interval)
+    np.testing.assert_allclose(
+        project, physical_interval, rtol=1e-4, atol=1e-8
+    )
+
+
+def test_interpolate(basis_data, physical_interval):
+    integration = arby.Integration(physical_interval)
+    basis = arby.Basis(basis_data, integration)
+
+    interpolation = basis.interpolate(physical_interval)
+    np.testing.assert_allclose(
+        interpolation, physical_interval, rtol=1e-4, atol=1e-8
+    )
+
+
+@pytest.mark.xfail
+def test_projectors():
+    """Test that projectors works as true projectors."""
+    npoints = 101
+    nu_train = np.linspace(1, 10, num=npoints)
+    x = np.linspace(0, 1, 1001)
+
+    # build traning space
+    training = np.array([BesselJ(nn, x) for nn in nu_train])
+
+    # build reduced basis
+    bessel = arby.ReducedOrderModel(
+        training_space=training,
+        physical_interval=x,
+        parameter_interval=nu_train,
+        greedy_tol=1e-12,
+    )
+
+    # compute a random index to test Proj_operator^2 = Proj_operator
+    random_index = np.random.randint(0, npoints)
+    proj_fun = bessel.project(training[random_index], bessel.basis)
+    proj2_fun = bessel.project(proj_fun, bessel.basis)
+    np.testing.assert_allclose(proj_fun, proj2_fun, rtol=1e-5, atol=1e-8)
+
+    # compute a random index to test Interpolant^2 = Interpolant
+    random_index = np.random.randint(0, npoints)
+
+    # build interpolant matrix
+    bessel.build_eim()
+    interp_fun = bessel.interpolate(training[random_index])
+    interp2_fun = bessel.interpolate(interp_fun)
+    np.testing.assert_allclose(interp_fun, interp2_fun, rtol=1e-5, atol=1e-8)
+
+
+@pytest.mark.xfail
+def test_projection_error():
+    """Test auto-consistency for projection error function."""
+    npoints = 101
+    nu_train = np.linspace(1, 10, num=npoints)
+    x = np.linspace(0, 1, 1001)
+
+    # build traning space
+    training = np.array([BesselJ(nn, x) for nn in nu_train])
+
+    # build reduced basis
+    bessel = arby.ReducedOrderModel(
+        training_space=training,
+        physical_interval=x,
+        parameter_interval=nu_train,
+        greedy_tol=1e-12,
+    )
+
+    # Check that projection errors of basis elements onto the basis is
+    # zero
+    computed_errors = [
+        bessel.projection_error(basis_element, bessel.basis)
+        for _, basis_element in enumerate(bessel.basis)
+    ]
+    expected_errors = [0.0] * bessel.Nbasis_
+    np.testing.assert_allclose(
+        computed_errors, expected_errors, rtol=1e-5, atol=1e-8
+    )
