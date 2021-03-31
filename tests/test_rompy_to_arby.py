1edb9f75fbbdfc34c1c522c1acfe1407d1344b52
JuanBC <jbc.develop@gmail.com>
2021-03-31 20:32:34 -0300

    all tests passes

--- tests/test_rompy_to_arby.py
+++ tests/test_rompy_to_arby.py
@@ -13,34 +13,31 @@ import arby
 
 import numpy as np
 
-import pytest
 
 # =============================================================================
 # TESTING
 # =============================================================================
 
 
-@pytest.mark.xfail
-def test_regression_reduced_basis(basis, training):
+def test_regression_reduced_basis(basis_data, training_space):
     "Test that the reduced basis matches ROMpy's for the same "
     "training data"
-    nbasis, npoints = basis.shape
-    physical_inteval = np.linspace(0, 1, npoints)
+    physical_interval = np.linspace(0, 1, basis_data.shape[-1])
 
     # build reduced basis
-    rb_bessel = arby.ReducedOrderModel(
-        training, physical_inteval, greedy_tol=1e-14
+    basis, _ = arby.reduce_basis(
+        training_space, physical_interval, greedy_tol=1e-14
     )
+
     # compare
-    np.testing.assert_allclose(rb_bessel.basis, basis, rtol=1e-5, atol=1e-8)
+    np.testing.assert_allclose(basis.data, basis_data, rtol=1e-5, atol=1e-8)
 
 
-@pytest.mark.xfail
-def test_regression_EIM(basis):
+def test_regression_EIM(basis_data, physical_interval):
     "Test that EIM matches ROMpy's for the same training data"
     rompy_eim_nodes = np.array([0, 100, 2, 36, 9, 72, 1, 20, 89, 4])
-    # Compute eim nodes for Bessel functions
-    eim_bessel = arby.ReducedOrderModel(basis=basis)
-    eim_bessel.build_eim()
-    # compare
-    np.testing.assert_array_equal(rompy_eim_nodes, eim_bessel.eim_nodes_)
+
+    integration = arby.Integration(physical_interval)
+    basis = arby.Basis(basis_data, integration)
+
+    np.testing.assert_array_equal(rompy_eim_nodes, basis.eim_.nodes)
