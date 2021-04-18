75539f9d9e2664dc5a69df94de47c90d9376f909
JuanBC <jbc.develop@gmail.com>
2021-04-18 18:37:39 -0300

    benchmarks done

---
+++ benchmark/benchmark.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021, Juan B Cabral and QuatroPe.
+# License: BSD-3-Clause
+#   Full Text: https://github.com/quatrope/pyonono/blob/master/LICENSE
+
+# This file is a prototype for the upcoming pyonono benchmark framework
+
+import arby
+
+import numpy as np
+
+import pytest
+
+# =============================================================================
+# BENCHMARK
+# =============================================================================
+
+
+@pytest.mark.parametrize("iteration", range(3))  # repite cada test 100 veces
+@pytest.mark.parametrize(
+    "training_set_shape",
+    [
+        (11, 11),
+        # (101, 101),
+        # (1001, 1001), (10001, 10001)
+    ],
+)
+@pytest.mark.parametrize(
+    "integration_rule", ["riemann", "trapezoidal", "euclidean"]
+)
+@pytest.mark.parametrize("greedy_tol", [1e-12])
+def test_benchmark_basis(
+    benchmark, training_set_shape, integration_rule, greedy_tol, iteration
+):
+    physical_points_size = training_set_shape[0]
+    physical_points = np.linspace(0, 1, physical_points_size)
+
+    training_set = np.random.random(training_set_shape)
+
+    benchmark(
+        arby.reduced_basis,
+        training_set=training_set,
+        physical_points=physical_points,
+        integration_rule=integration_rule,
+        greedy_tol=greedy_tol,
+    )
