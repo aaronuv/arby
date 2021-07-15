f5f6ac7a2a718ba9223e75ca0748de8ad4b57f0f
JuanBC <jbc.develop@gmail.com>
2021-07-15 17:54:14 -0300

    benchmark configured

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -18,9 +18,9 @@ import pytest
 # =============================================================================
 
 
-@pytest.mark.parametrize("iteration", range(100))  # repite cada test 100 veces
+@pytest.mark.parametrize("iteration", range(1000))  # repite cada test 100 veces
 @pytest.mark.parametrize(
-    "training_set_shape",
+    "training_set_shape",  # b
     [
         (11, 11),
         (101, 101),
@@ -31,11 +31,12 @@ import pytest
 @pytest.mark.parametrize(
     "integration_rule", ["riemann", "trapezoidal", "euclidean"]
 )
-@pytest.mark.parametrize("greedy_tol", [1e-12])
+@pytest.mark.parametrize("greedy_tol", [1e-12, [1e-14]])
+@pytest.mark.parametrize("normalize", [True, False])
 def test_benchmark_basis(
-    benchmark, training_set_shape, integration_rule, greedy_tol, iteration
+    benchmark, training_set_shape, integration_rule, greedy_tol, iteration, normalize
 ):
-    physical_points_size = training_set_shape[0]
+    physical_points_size = training_set_shape[1]
     physical_points = np.linspace(0, 1, physical_points_size)
 
     training_set = np.random.random(training_set_shape)
@@ -46,4 +47,5 @@ def test_benchmark_basis(
         physical_points=physical_points,
         integration_rule=integration_rule,
         greedy_tol=greedy_tol,
+        normalize=normalize
     )
